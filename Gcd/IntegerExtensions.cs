14a00aae871084bcea237bd691f64f9416530af5
Alexander <154324-alexander-rykov@users.noreply.gitlab.com>
2022-02-08 14:30:19 +0300

    Add solution configuration files: editor configs, stylecop settings, style ruleset, readme. Add projects with unit tests.

---
+++ Gcd/IntegerExtensions.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace GcdTask
+{
+    public static class IntegerExtensions
+    {
+        /// <summary>
+        /// Calculates GCD of two integers from [-int.MaxValue;int.MaxValue]  by the Euclidean algorithm.
+        /// </summary>
+        /// <param name="a">First integer.</param>
+        /// <param name="b">Second integer.</param>
+        /// <returns>The GCD value.</returns>
+        /// <exception cref="ArgumentException">Thrown when all numbers are 0 at the same time.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
+        public static int FindGcd(int a, int b)
+        {
+            throw new NotImplementedException("You need to implement this method.");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## Task description ##
+
+Implement a [FindGcd](GcdTask/IntegerExtensions.cs#L15) method to find the [GCD](https://en.wikipedia.org/wiki/Greatest_common_divisor) of two integers. _The task definition is given in the XML-comments for the method._
+
+*Topics - algorithms*.
