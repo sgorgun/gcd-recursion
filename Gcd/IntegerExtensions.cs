47fc6f9fb999a6027c637f52d4ede925374d6948
@gorgun.sergei <gorgun.sergei@student.ehu.lt>
2022-12-11 02:50:50 +0200

    Ready

--- Gcd/IntegerExtensions.cs
+++ Gcd/IntegerExtensions.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace GcdTask
 {
@@ -14,7 +14,46 @@ namespace GcdTask
         /// <exception cref="ArgumentOutOfRangeException">Thrown when one or two numbers are int.MinValue.</exception>
         public static int FindGcd(int a, int b)
         {
-            throw new NotImplementedException("You need to implement this method.");
+            if (a == 0 && b == 0)
+            {
+                throw new System.ArgumentException("Two numbers cannot be 0 at the same time.");
+            }
+
+            if (a is int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(a), $"{nameof(a)} can't be int.MinValue.");
+            }
+
+            if (b is <= int.MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(b), $"{nameof(b)} can't be int.MinValue.");
+            }
+
+            int y = GcdNumber(a, b);
+            if (y > 0)
+            {
+                return y;
+            }
+            else
+            {
+                return -y;
+            }
+        }
+
+        private static int GcdNumber(int x, int y)
+        {
+            if (y == 0)
+            {
+                return x;
+            }
+
+            int z = x % y;
+            if (z == 0)
+            {
+                return y;
+            }
+
+            return GcdNumber(y, z);
         }
     }
 }
